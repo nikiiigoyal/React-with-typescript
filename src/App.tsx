62afe497df3755ccff6559b325e8c7876d7cf888
nikiiigoyal <nikiiigoyal@gmail.com>
2025-06-13 10:24:09 +0530

    add new challenge in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 
-import Component from "./components/01-return";
-import Props from "./components/02-props";
-import State from "./components/03-state";
-import Events from "./components/04-events";
-import Profile from "./components/05-advancedprops";
-import ParentComponent from "./components/06-context";
-import Reducers from "./components/07-reducers";
-import FetchingData from "./components/08-datafetch";
+// import Component from "./components/01-return";
+// import Props from "./components/02-props";
+// import State from "./components/03-state";
+// import Events from "./components/04-events";
+// import Profile from "./components/05-advancedprops";
+// import ParentComponent from "./components/06-context";
+// import Reducers from "./components/07-reducers";
+// import FetchingData from "./components/08-datafetch";
+import MainTasks from "./components/09- tasks";
 
 function App() {
   return (
     <main>
-     <Component />
+     {/* <Component />
      <Props name='peter' id={12} children />
      <p>This is a prop passed as a children</p>
      <State />
@@ -19,7 +20,8 @@ function App() {
      <Profile type="advanced"  name='niki' email="nidnn@gmail.com"/>
      <ParentComponent />
      <Reducers />
-     <FetchingData />
+     <FetchingData /> */}
+     <MainTasks />
     </main>
   );
 }
